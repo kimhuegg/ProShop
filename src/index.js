8933812ea38df3e592ea501080d3c33928b60c4c
kimhuegg <kimhuegg@gmail.com>
2022-04-26 10:56:24 +0700

    fix smt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
@@ -7,15 +8,15 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { store } from './app/store'
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
 
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </Provider>, document.getElementById("root")
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
